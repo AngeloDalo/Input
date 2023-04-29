92fce2c19f0699b5c5a64d004af976f469ff6e5a
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-04-29 09:37:34 +0200

    Aggiungere i file di progetto.

---
+++ Input/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Corso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero");
+            string inputUtente = Console.ReadLine();
+            Console.WriteLine("Inserisci un altro numero");
+            string inputUtente2 = Console.ReadLine();
+            int num1 = int.Parse(inputUtente);
+            int num2 = int.Parse(inputUtente2);
+            int risultato = num1 + num2;
+            Console.WriteLine("risultato: " + risultato);
+        }
+    }
+}
